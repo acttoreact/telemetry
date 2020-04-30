a441c13ba567ae363bdda165b372c6e051677870
Ignacio Ferro Picón <nacho@binpar.com>
2020-04-30 18:02:44 +0200

    fixes

--- tools/out.ts
+++ tools/out.ts
@@ -1,13 +1,28 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import chalk from 'chalk';
 import winston, { format } from 'winston';
 
+/**
+ * Used during the transformation pipeline
+ */
 interface TransformableInfo {
+  /**
+   * Log level
+   */
   level: string;
+  /**
+   * Message
+   */
   message: string;
 }
 
-// Function to remove the colors for the message
+/**
+ * Log level options (in order of relevance)
+ */
+type LogLevel = 'error'|'warn'|'info'|'verbose'|'debug';
+
+/**
+ * Function removes the colors for the message
+ */
 const removeColors = format(
   (info: TransformableInfo): TransformableInfo => {
     const newInfo = { message: info.message, level: info.level };
@@ -23,7 +38,9 @@ const removeColors = format(
   },
 );
 
-// File transport for both error and combined logs
+/**
+ * Winston transport model definition
+ */
 const transports = [
   new winston.transports.File({
     filename: 'log/error.log',
@@ -36,18 +53,26 @@ const transports = [
   }),
 ];
 
+/**
+ * Set the default level depending of the environment:
+ * verbose for development and info por production 
+ */
 const defaultLevel =
   process.env.NODE_ENV === 'development' ? 'verbose' : 'info';
 
-// Winston logger
+/**
+ * Create the winston logger
+ */
 const logger = winston.createLogger({
   level: defaultLevel,
   format: format.json(),
   transports,
 });
 
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+/**
+ * If we're not in production then log to the `console` with the format:
+ * `${info.level}: ${info.message} JSON.stringify({ ...rest })`
+ */
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
@@ -55,29 +80,61 @@ if (process.env.NODE_ENV !== 'production') {
     }),
   );
 }
-
+/**
+ * Prefix to delete previous chars
+ */
 const prefix = ''.padStart(10, '\b');
 
-export const info = (message: string, meta?: any): void => {
+/**
+ * Logs information
+ * @param {string} message message to log
+ * @param {object} meta metadata associated with the event
+ */
+export const info = (message: string, meta?: object): void => {
   logger.info(`${prefix}${message}`, meta);
 };
 
-export const error = (message: string, meta?: any): void => {
+/**
+ * Logs an error
+ * @param {string} message error to log
+ * @param {object} meta metadata associated with the event
+ */
+export const error = (message: string, meta?: object): void => {
   logger.error(`${prefix}${chalk.red.bold('[ERROR]')} ${message}`, meta);
 };
 
-export const warn = (message: string, meta?: any): void => {
+/**
+ * Logs a warning
+ * @param {string} message warning to log
+ * @param {object} meta metadata associated with the event
+ */
+export const warn = (message: string, meta?: object): void => {
   logger.warn(`${prefix}${chalk.yellow.bold('[WARN]')} ${message}`, meta);
 };
 
-export const alert = (message: string, meta?: any): void => {
+/**
+ * Logs an alert
+ * @param {string} message alert to log
+ * @param {object} meta metadata associated with the event
+ */
+export const alert = (message: string, meta?: object): void => {
   logger.error(`${prefix}${chalk.red.bold('[ALERT]')} ${message}`, meta);
 };
 
-export const verbose = (message: string, meta?: any): void => {
+/**
+ * Logs verbose information
+ * @param {string} message information to log
+ * @param {object} meta metadata associated with the event
+ */
+export const verbose = (message: string, meta?: object): void => {
   logger.verbose(`${prefix}${message}`, meta);
 };
 
-export const setLevel = (level: string): void => {
+/**
+ * Filters the messages to log for a certain level
+ * @param {LogLevel} level minimum log level to be displayed that can be
+ * in order of relevance: error, warn, info, verbose or debug.
+ */
+export const setLevel = (level: LogLevel): void => {
   logger.level = level;
 };
